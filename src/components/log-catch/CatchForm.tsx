3a088434723c792ebebd2cf269db20def28f932e
Manish Shetty <mmanishrshetty@gmail.com>
2025-07-13 12:25:52 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/log-catch/CatchForm.tsx
+++ src/components/log-catch/CatchForm.tsx
@@ -6,6 +6,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { format } from "date-fns";
 import { useRouter } from "next/navigation";
+import { motion } from "framer-motion";
 
 import { Button } from "@/components/ui/button";
 import {
